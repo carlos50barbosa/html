6903e2cc4a4c7f643e6e29a629ec6f5715e462c2
José Carlos Barbosa <127363766+carlos50barbosa@users.noreply.github.com>
2024-04-28 23:42:37 -0300

    v4

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src="../img/menu_white.svg";
+        document.querySelector('.icon').src="menu_white.svg";
 
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src="../img/close_white.svg";
+        document.querySelector('.icon').src="close_white.svg";
         
         
 
